c9d444e61c0769de74159a4a1868802741dce8a9
You-now-Who <yashyadav.sr@gmail.com>
2023-02-19 11:12:23 +0530

    Added stuff

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -7,25 +7,122 @@ import "react-draft-wysiwyg/dist/react-draft-wysiwyg.js";
 
 export default function Demo() {
     
+    let x = ""
+
+    let inclusiveAlerts = []
+
+    let filterDict = {
+        "he": ["they", "gender neutral"],
+        "she": ["they", "gender neutral"],
+        "her": ["them", "gender neutral"],
+        "him": ["them", "gender neutral"],
+        "his": ["their", "gender neutral"],
+        "hers": ["theirs", "gender neutral"],
+        "himself": ["themself", "gender neutral"],
+        "herself": ["themself", "gender neutral"],
+        "guys": ["folks/people/y'all", "gender neutral"],
+
+
+    }
+
     const [editorState, setEditorState] = useState(
         () => EditorState.createEmpty(),
       );
     
+    // function to handle the click event of the button
+    const handleClick = () => {
+        x = editorState.getCurrentContent().getPlainText();
+        console.log(x);
+        // Set the editorState plain text to x
+        setEditorState( EditorState.createWithContent(x));
+
+    }
+
+    const returnInclusiveAlerts = (text) => {
+        let inclusiveAlerts = []
+        text = text.toLowerCase()
+
+        // remove all punctuation from text
+        text = text.replace(/[.,\/#!$?%\^&\*;:{}=\-_`~()]/g, "")
+        console.log(text)
+
+        let textArray = text.split(" ")
+        for (let i = 0; i < textArray.length; i++) {
+            if (textArray[i] in filterDict) {
+                inclusiveAlerts.push(textArray[i])
+            }
+        }
+        return inclusiveAlerts
+
+    }
+
+    const onEditorChange = (editorState) => {
+        setEditorState(editorState);
+        x = editorState.getCurrentContent().getPlainText()
+        // console.log(x);
+        // console.log(returnInclusiveAlerts(x))
+
+        inclusiveAlerts = returnInclusiveAlerts(x)
+
+        let addCard = document.getElementById('addCard')
+        addCard.innerHTML = ""
+        let i = 0;
+        for (i in inclusiveAlerts) {
+            console.log(filterDict[inclusiveAlerts[i]])
+            
+            addCard.innerHTML += `<div class="bg-white p-6 rounded-lg shadow-lg m-2">
+                    <h2 class="text-2xl font-bold mb-2 text-gray-800">` + inclusiveAlerts[i] + `</h2>
+                    <p class="text-gray-700">We suggest to use a more <strong>`+ filterDict[inclusiveAlerts[i]][1] +`</strong> term: '<strong>` + filterDict[inclusiveAlerts[i]][0] + `</strong>'</p>
+                </div>  `
+
+        }
+        let alTitle = document.getElementById('alTitle')
+        alTitle.innerHTML = "Inclusive Alerts: " + inclusiveAlerts.length
+    }
+    
     return (
     <>
-        <div className="flex flex-col items-center min-h-screen">
-            <div className='pt-4'>
-                <Editor 
+
+        <div className="flex flex-row items-center justify-center">
+            <h1 className="text-5xl self-start font-bold my-5 font-roboto">Inkling</h1>
+        </div>
+        
+        <div className="flex flex-row items-center justify-center">
+            <div className='pt-4 mr-40 self-start'>
+                <Editor
+                    spellCheck
                     editorState={editorState}
                     toolbarClassName="toolbarClassName"
                     wrapperClassName="wrapperClassName"
                     editorClassName="editorClassName"
-                    onEditorStateChange={setEditorState}
-                    wrapperStyle={{ width: 600, height: 500, borderRight: "1px grey",  }}
+                    editorStyle={{ backgroundColor: "white", padding: 10 }}
+                    onEditorStateChange={onEditorChange}
+                    wrapperStyle={{ width: 700, height: 560, borderRight: "1px grey",  }}
                 />
-            </div>
                 
+            </div>
+
+            
+            <div className='pt-4 pr-10 self-end items-end w-max overflow-y-scroll' style={{height: 600, width: 550}}>
+
+            <h1 className="text-4xl self-end " id="alTitle">Inclusive Alerts: 0</h1>
+                    
+
+                    <div className="flex flex-col mt-10" id='addCard'>
+                            {/* <div className="bg-white p-6 rounded-lg shadow-lg m-2">
+                                <h2 className="text-2xl font-bold mb-2 text-gray-800">Card with no image</h2>
+                                <p className="text-gray-700">This is my cool new card 1!</p>
+                            </div>  
+                            <div className="bg-white p-6 rounded-lg shadow-lg m-2">
+                                <h2 className="text-2xl font-bold mb-2 text-gray-800">Card with no image</h2>
+                                <p className="text-gray-700">This is my cool new card 1!</p>
+                            </div>   */}
+                        
+                    </div>    
+                    
+            </div>
         </div>
+        
 
     </>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,8 @@ function Navbar() {
 <nav className="p-3 border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
   <div className="container flex flex-wrap items-center justify-between mx-auto">
     <a href="#" className="flex items-center">
-        <img src="https://i.imgur.com/phoefgm.png" className="h-6 mr-3 sm:h-10 rounded" alt="Inkling Logo" />
-        {/* <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Inkling</span> */}
+        {/* <img src="https://i.imgur.com/phoefgm.png" className="h-6 mr-3 sm:h-10 rounded" alt="Inkling Logo" /> */}
+        <span className="self-center text-3xl font-semibold whitespace-nowrap dark: text-white">Inkling</span>
     </a>
     <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
